b2c2d1a83d5d5e108e5f31abf779587970b7b063
Jordan Davila <hi@jordandavila.com>
2021-08-15 11:43:51 -0400

    Init Commit

---
+++ components/AppLayout.js
@@ -0,0 +1,11 @@
+export default function AppLayout(props) {
+    return (
+        <div>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Sanity + Next.js = 💖</title>
+            </Head>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    darkMode: false,
+    theme: {
+        fontFamily: {},
+        colors: {
+            gray: {
+                50: "#F7F8FD",
+                100: "#F2F4FF",
+                200: "#647196",
+                300: "#3A4374",
+                400: "#373F68",
+            },
+            purple: {
+                500: "#AD1FEA",
+            },
+            indigo: {
+                500: "#4661E6",
+            },
+        },
+        extend: {
+            colors: {
+                tacao: "#F49F85",
+                malibu: "#62BCFA",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
